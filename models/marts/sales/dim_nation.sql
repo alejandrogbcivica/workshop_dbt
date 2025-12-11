8872ef40546da317201cedba994ee0fdf6476619
Alejandro González Bueno <alejandro.gonzalez@civica-soft.com>
2025-12-11 12:56:11 +0000

    testacy

--- models/marts/sales/dim_nation.sql
+++ models/marts/sales/dim_nation.sql
@@ -7,12 +7,5 @@ with stage as (
     from {{ ref('stg_tpch__nation') }}
 )
 
-select
-    id_nation
-    , id_region
-    , nation_key
-    , region_key
-    , nation_name
-    , nation_comment
-    , convert_timezone('UTC', current_timestamp()) as staged_at_utc
+select *
 from stage
